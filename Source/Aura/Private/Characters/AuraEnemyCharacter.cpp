02994c3d6ea21e50af6315b113d4c56b94b20228
Bloodrushh <rustamshamaev@yandex.ru>
2023-11-16 20:41:17 +0700

    Implement ability system interface to player state and base character classes

--- Source/Aura/Private/Characters/AuraEnemyCharacter.cpp
+++ Source/Aura/Private/Characters/AuraEnemyCharacter.cpp
@@ -3,9 +3,17 @@
 
 #include "Characters/AuraEnemyCharacter.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
 AAuraEnemyCharacter::AAuraEnemyCharacter()
 {
 	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");	
 }
 
 void AAuraEnemyCharacter::HighlightActor()
